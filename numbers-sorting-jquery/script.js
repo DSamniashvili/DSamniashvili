4a4d5ffd78ec7eda44c5648dc941d87e775597e8
DSamniashvili <38917781+DSamniashvili@users.noreply.github.com>
2018-08-13 14:42:02 +0400

    Update script.js

--- numbers-sorting-jquery/script.js
+++ numbers-sorting-jquery/script.js
@@ -1,91 +1,130 @@
 let numArray = [];
-let $div = $('#numbers');
-for (let i = 1; i <= 150; i++){
-   numArray.push(i);
+for(let i=0; i<=150; i++) {
+    numArray.push(i)
 }
+// console.log(numArray)
+let numbersWrapper = document.querySelector('#numbers');
 
-function change(){
-$div.empty();
-let modified = []; 
-$div.append( numArray.map((el) => {
-return `<div id="each-number">${el}</div>`
-   }).join('')
-);
+function generateNumbers(){
+    for(let i=0; i<numArray.length; i++) {
+        
+        let item = document.createElement('div');
+        item.className = 'each-number';
+        item.innerHTML = numArray[i];
+        numbersWrapper.appendChild(item)
 
-let deletedItemsArray = [];
-
-
-let eachDiv = document.querySelectorAll('#each-number');
-    // console.log(eachDiv.length)
-    // console.log(eachDiv)
-for(let i=0; i< eachDiv.length; i++){
-    // console.log(eachDiv[i])
-    eachDiv[i].addEventListener('click', function(){
-        // let index = eachDiv[i];
-        // console.log("hello")
-        eachDiv[i].parentNode.removeChild(eachDiv[i])
-        deletedItemsArray.push(eachDiv[i].innerHTML);
-        return deletedItemsArray;
-    })
-    
+        item.addEventListener('click', function(){
+            item.parentNode.removeChild(item)
+        })
+    }
 }
-console.log(deletedItemsArray);
-
-// function diffArray(arr1, arr2) {
-//     return arr1.concat(arr2).filter(function (val) {
-//         if ((arr1.includes(val) && !arr2.includes(val)))
-//             return val;
-//     });
-    
-//  }
-// let different = diffArray(numArray, deletedItemsArray);
-// console.log(different.length)
-
-
-// modified.length = numArray.length - deletedItemsArray.length
-// console.log(modified.length);
-
-// console.log(deletedItemsArray)
-}
-
-
-
-
-window.onload = change;
+generateNumbers();
 
 function shuffle(){
-   function shuffle(array) {
+    numbersWrapper.innerHTML = ''
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
+  let shuffled = shuffle(numArray)
+  generateNumbers();
+}
+
+function ascend(){
+    numbersWrapper.innerHTML = ''
+    numArray.sort((a,b) => {
+        return a - b;
+    })
    
-       var currentIndex = array.length, temporaryValue, randomIndex;
-       while (0 !== currentIndex) {
-         // Pick a remaining element...
-         randomIndex = Math.floor(Math.random() * currentIndex);
-         currentIndex -= 1;
+    generateNumbers();
+ } ascend()
+ 
+ function descend() {
+ numbersWrapper.innerHTML = ''
+    numArray.sort((a,b) => {
+        return b - a;
+    })
+    generateNumbers();
+ }
+
+
+  
+
+// let numArray = [];
+// let $div = $('#numbers');
+// for (let i = 1; i <= 150; i++){
+//    numArray.push(i);
+// }
+
+// function change(){
+// $div.empty();
+// let modified = []; 
+// $div.append( numArray.map((el) => {
+// return `<div id="each-number">${el}</div>`
+//    }).join('')
+// );
+
+// let deletedItemsArray = [];
+
+
+// let eachDiv = document.querySelectorAll('#each-number');
+//     // console.log(eachDiv.length)
+//     // console.log(eachDiv)
+// for(let i=0; i< eachDiv.length; i++){
+//     // console.log(eachDiv[i])
+//     eachDiv[i].addEventListener('click', function(){
+//         // let index = eachDiv[i];
+//         // console.log("hello")
+//         eachDiv[i].parentNode.removeChild(eachDiv[i])
+//         deletedItemsArray.push(eachDiv[i].innerHTML);
+//         return deletedItemsArray;
+//     })
+    
+// }
+// console.log(deletedItemsArray);
+
+// window.onload = change;
+
+// function shuffle(){
+//    function shuffle(array) {
+   
+//        var currentIndex = array.length, temporaryValue, randomIndex;
+//        while (0 !== currentIndex) {
+//          // Pick a remaining element...
+//          randomIndex = Math.floor(Math.random() * currentIndex);
+//          currentIndex -= 1;
      
-         // And swap it with the current element.
-         temporaryValue = array[currentIndex];
-         array[currentIndex] = array[randomIndex];
-         array[randomIndex] = temporaryValue;
-       }
-       return array;
-      }
-      arr = shuffle(numArray);
-      console.log(arr);
-      change();
+//          // And swap it with the current element.
+//          temporaryValue = array[currentIndex];
+//          array[currentIndex] = array[randomIndex];
+//          array[randomIndex] = temporaryValue;
+//        }
+//        return array;
+//       }
+//       arr = shuffle(numArray);
+//       console.log(arr);
+//       change();
 
      
-}
-function ascend(){
-   numArray.sort((a,b) => {
-       return a - b;
-   })
-   change();
-}
-function descend()
-{
-   numArray.sort((a,b) => {
-       return b - a;
-   })
-   change();
-}
+// }
+// function ascend(){
+//    numArray.sort((a,b) => {
+//        return a - b;
+//    })
+//    change();
+// }
+// function descend()
+// {
+//    numArray.sort((a,b) => {
+//        return b - a;
+//    })
+//    change();
+// }
 
