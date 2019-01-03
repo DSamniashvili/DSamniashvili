e46d799beba9d87acae376ff4e64e878e42ef9e7
Dea <deasamniashvili@gmail.com>
2019-01-03 13:45:32 +0400

    add project3

---
+++ build/precache-manifest.d1be6805b9db0ba7dba5b6013ec97bfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "03943edbddf8bf7c01a4",
+    "url": "/static/js/main.03943edb.chunk.js"
+  },
+  {
+    "revision": "a0e5d0203fb5f9cc4a4c",
+    "url": "/static/js/1.a0e5d020.chunk.js"
+  },
+  {
+    "revision": "03943edbddf8bf7c01a4",
+    "url": "/static/css/main.d0e26c32.chunk.css"
+  },
+  {
+    "revision": "a0e5d0203fb5f9cc4a4c",
+    "url": "/static/css/1.eada9a88.chunk.css"
+  },
+  {
+    "revision": "2057d5cc7669df7f1dae94d011e5b2f4",
+    "url": "/index.html"
+  }
+];
